6c07c88c7ab017d8dbcb8b003c75debb9a7d1e37
Sadow0x <105595476+Sadow0x@users.noreply.github.com>
2024-04-20 12:03:15 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,20 @@
-# install flask from pip3
-package { 'flask' :
-    ensure   => '2.1.0',
-    provider => 'pip3',
-    }
+# Puppet script to install Flask version 2.1.0
+# This script installs Flask version 2.1.0 using pip3
+
+# Install Flask package
+package { 'python3-flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+# Ensure flask command is available
+exec { 'link_flask_command':
+  command => '/usr/local/bin/python3 -m pip install --upgrade Flask==2.1.0',
+  creates => '/usr/local/bin/flask',
+}
+
+# Ensure werkzeug library is updated
+package { 'python3-werkzeug':
+  ensure => latest,
+  provider => 'pip3',
+}
