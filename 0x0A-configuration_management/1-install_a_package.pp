0e784a6b190ffcbb07a6f2dcbed44e49a821fa28
Sadow0x <105595476+Sadow0x@users.noreply.github.com>
2024-04-20 01:37:56 +0100

    Add files via upload

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# Install Flask package
-package { 'python3-flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-}
+# install flask from pip3
+package { 'flask' :
+    ensure   => '2.1.0',
+    provider => 'pip3',
+    }
